eaadaa8ddee9d749a412dba97f537c96e245e660
Gordon <gjrevans@gmail.com>
2015-04-05 19:47:16 -0400

    Set up the flash messsages with classes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   #Set the config for the action mailer stuffs
-  config.action_mailer.default_url_options = { :host => 'http://gordon-payments.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'http://someurl.herokuapp.com/' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
